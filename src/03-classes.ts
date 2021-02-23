da24d2ad2135894afcca0387242748cb08522686
Ruben Sibon <r.sibon@amsterdam.nl>
2021-02-23 15:09:15 +0100

    Add more examples of class implementation

--- src/03-classes.ts
+++ src/03-classes.ts
@@ -1,6 +1,8 @@
 console.group("03-CLASSES");
 
-class OrgUnit {
+type DateNull = Date | null;
+
+abstract class OrgUnit {
   // Example of a protected and read-only property.
   protected readonly id: string;
 
@@ -19,6 +21,10 @@ class OrgUnit {
   get getName() {
     return this.name;
   }
+
+  // Example of an abstract method that should exist on all derived (extended) classes,
+  // but that may have varying implementations.
+  abstract describe(this: OrgUnit): void;
 }
 
 // Example of class inheritance (extension).
@@ -29,13 +35,14 @@ class Employee extends OrgUnit {
     // Examples of a private property.
     protected name: string,
 
-    private dateStarted: Date | null,
-    private dateEnded: Date | null = null
+    private dateStarted: DateNull,
+    private dateEnded: DateNull = null
   ) {
     super(name);
 
     this.id = `empl${this.name}${Math.round(Math.random() * 100000)}`;
   }
+
 
   get getDateStarted() {
     return this.dateStarted;
@@ -43,6 +50,18 @@ class Employee extends OrgUnit {
 
   get getDateEnded() {
     return this.dateEnded;
+  }
+
+  // Nonsensical example of a static method that should be called on the constructor/class and not an instance.
+  static createEmployee(name: string, dateStarted: DateNull, dateEnded: DateNull) {
+    return new Employee(name, dateStarted, dateEnded);
+  }
+
+  describe() {
+    return `
+${this.constructor.name} name: ${this.getName};
+${this.constructor.name} ID: ${this.getId};
+`;
   }
 }
 
@@ -59,17 +78,26 @@ class Department extends OrgUnit {
     this.id = `dept${this.name}${Math.round(Math.random() * 100000)}`;
   }
 
-  // `this` is a special type of parameter in TypeScript which tells the compiler what type of class this method can be called on.
-  describe(this: Department) {
-    return `Department: ${this.name}`;
+  // Example of a static property that exists on the class.
+  static fiscalYear = 2021;
+
+  describe() {
+    return `
+${this.constructor.name} name: ${this.getName};
+${this.constructor.name} ID: ${this.getId};
+`;
   }
 
   addEmployee(employee: Employee) {
     this.employees = [...this.employees, employee];
   }
 
-  getEmployeeNames() {
-    return this.employees.map(empl => empl.getName);
+  getEmployeeNames(activeOnly = false) {
+    const filteredEmployees = activeOnly
+      ? this.employees.filter(empl => !empl.getDateEnded && empl.getDateEnded! <= new Date())
+      : this.employees;
+
+    return filteredEmployees.map(empl => empl.getName);
   }
 
   getEmployeeInfo() {
@@ -78,7 +106,7 @@ class Department extends OrgUnit {
 Employees in ${this.name}:
 Number of employees: ${this.employees.length}.
 All the employees:
-${this.getEmployeeNames()}
+${this.getEmployeeNames(true)}
 ========================
     `;
   }
@@ -95,11 +123,19 @@ deptMarketing.addEmployee(emplRuben);
 deptAccounting.addEmployee(emplDaan);
 deptMarketing.addEmployee(emplIsaac);
 
+// Add a "new" employee using the static method.
+const emplMax = Employee.createEmployee("Max", new Date("2019-05-20"), new Date("2021-01-15"));
+deptAccounting.addEmployee(emplMax);
+
+// Access a static property on a class. Static props cannot be accessed from instances derived from the class!
+console.log(Department.fiscalYear);
+
 console.log("Instance 1 of a department:\n", deptMarketing.describe(), deptMarketing);
 console.log(deptMarketing.getEmployeeInfo());
 
 console.log("Instance 2 of a department:\n", deptAccounting.describe(), deptAccounting);
 console.log(deptAccounting.getEmployeeInfo());
+console.log("Employees in accounting that are active", deptAccounting.getEmployeeNames(true));
 
 console.log(deptAccounting.getName, deptAccounting.getId);
 console.log(emplRuben.getName, emplRuben.getId);
