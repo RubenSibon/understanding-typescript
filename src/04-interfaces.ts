4bacb498ae1a1c7d79e4895fdbaf67edde29039f
Ruben Sibon <r.sibon@amsterdam.nl>
2021-02-24 17:01:38 +0100

    Add code to extend and implement interfaces

--- src/04-interfaces.ts
+++ src/04-interfaces.ts
@@ -1,14 +1,23 @@
 console.group("04-INTERFACES");
 
-interface Greetable {
+interface Named {
   name: string;
-
-  greet(phrase: string): void;
 }
 
-class Person implements Greetable {
+interface Greetable extends Named {
+  greet(phrase: string): string;
+}
+
+interface Mortal {
+  readonly isMortal: boolean,
+
+  kill(): string;
+}
+
+class Person implements Greetable, Mortal {
   name: string;
   age: number;
+  isMortal: boolean;
 
   constructor(
     name: string,
@@ -16,14 +25,20 @@ class Person implements Greetable {
   ) {
     this.name = name;
     this.age = age;
+    this.isMortal = true;
   }
 
   greet(phrase: string) {
     return `${phrase} ${this.name}`;
   }
+
+  kill() {
+    return `${this.name} has been killed!`;
+  }
 }
 
-let user1: Greetable;
+// let user1: Greetable; // Greetable interfaces does not have the Mortal interface so it fails when we want to call `kill()` on it later.
+let user1: Person;
 
 // ...sometime later...
 
@@ -31,5 +46,6 @@ user1 = new Person("Ruben", 34);
 
 console.log(user1);
 console.log(user1.greet("Hello"));
+console.log(user1.kill()); // Would fail if `user1` was constructed according to the Greetable interface only.
 
 console.groupEnd();
